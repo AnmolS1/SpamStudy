9fa0540d2bf4ea85ee183852ebdf4e84798d945c
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-01 20:02:33 -0800

    basic implementation
    completed basic implementation

--- testing/test.py
+++ testing/test.py
@@ -30,7 +30,7 @@ def run():
 	driver.find_element(By.XPATH, '//*[@id="identifierNext"]').click()
 	
 	# sleep for a bit because login do be takin a hot sec to load
-	sleep(5)
+	sleep (5)
 	
 	# find the password input box, send the password
 	driver.find_element(By.XPATH, '//input[@type="password"]').send_keys(password)
@@ -38,51 +38,60 @@ def run():
 	driver.find_elements(By.TAG_NAME, 'button')[1].click()
 	
 	# sleep for a bit again, another hot sec for loading
-	sleep(7)
+	sleep (7)
 	
 	# we gotta hit the down button because the spam tab isn't shown in the
 	# upper list
 	driver.find_element(By.XPATH, '//span[@role="button"]').click()
 	# sleep for 2 seconds bc load time
-	sleep(2)
+	sleep (2)
 	# boom baby we in da spam box
 	spam_inbox = driver.find_element(By.LINK_TEXT, 'Spam')
 	spam_inbox.click()
 	
 	# need lots of sleeping here otherwise google puts up some bs authentication issue
-	sleep(5)
+	sleep (5)
 	# get the 3rd table of emails (don't ask why bc i don't know) and then get the emails as a list
 	email_list = driver.find_elements(By.XPATH, '//table[@role="grid"]')[3].find_elements(By.XPATH, './/tr[@role="row"]')
+	if len(email_list) == 0:
+		print("FIGURE OUT WHAT TO DO HERE")
+		driver.delete_all_cookies()
+		driver.quit()
+		# add 0s to the database
 	# sleep again
-	sleep(1)
+	sleep (1)
 	
-	
-	# TEMPORARY CODE, TESTING USAGE IMPLEMENTATION
-	# just some stuff to keep track once we get to that step
 	user_filtered = 0
 	gmail_filtered = 0
 	
-	# click on the first email, we're just testing here
 	email_list[0].click()
-	# sleep, even if we're testing, we don't wanna run into any authentication problems
-	sleep(1)
+	while True:
+		sleep (3)
+
+		label = driver.find_element(By.XPATH, '//h2/following-sibling::p')
+		heading = driver.find_element(By.XPATH, '//p/preceding-sibling::h2')
+		if "dangerous" in heading.text:
+			gmail_filtered = gmail_filtered + 1
+		else:
+			if "You reported" in label.text:
+				user_filtered = user_filtered + 1
+			else:
+				gmail_filtered = gmail_filtered + 1
+
+		sleep (2)
+		
+		el = driver.find_elements(By.XPATH, '//div[@role="button" and @aria-label="Older"]')[2]
+		if el.get_attribute('aria-disabled') == "true":
+			break
+		else:
+			el.click()
 	
-	# get the list of the headers of each label as well as the list of the paragraphs following those labels
-	label_list = driver.find_elements(By.XPATH, '//h2/following-sibling::p')
-	heading_list = driver.find_elements(By.XPATH, '//p/preceding-sibling::h2')
-	
-	# categorization will happen here
-	
-	
-	# sleep another second
-	sleep(1)
-	# head back to the spam inbox and restart the process
-	spam_inbox.click()
+	print(" user filtered: " + str(user_filtered) + " email" + ("s" if user_filtered != 1 else ""))
+	print("gmail filtered: " + str(gmail_filtered) + " email" + ("s" if gmail_filtered != 1 else ""))
  
-	
-	
 	# delete the cookies, can't have our browser out here gaining weight
 	driver.delete_all_cookies()
+	driver.quit()
 	# temp line for testing, basically just keeps the browser open
 	sleep(1000)
 
