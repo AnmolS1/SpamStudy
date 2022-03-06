d4be95deea6c674bc1ef839d0d5b6f4a2207a0b7
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-06 02:55:21 -0800

    complete runner.sh
    i think the runner file is finished. if homebrew isn't installed, this installs it. if xterm isn't installed, this installs it. if python isn't installed, this installs it. if pip isn't installed, this installs it. if selenium or undetected_chromedriver isn't installed, this installs it. then this runs the python script in an xterm window. all that's left is to make this an executable :)

--- runner.sh
+++ runner.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# install homebrew if it doesn't exist
+if [ ! "$(command -v brew)" ]; then
+    if [[ -e "homebrew" ]]; then
+        rm -rf homebrew
+    fi
+    mkdir homebrew
+    git clone https://github.com/Homebrew/brew homebrew
+    eval "$(homebrew/bin/brew shellenv)" >> installation.log
+    brew update --force --update >> installation.log
+fi
+
+if [ ! "$(command -v xterm)" ]; then
+    brew install xterm >> installation.log
+fi
+
 # let user install python if not already there
 if [[ ! "$(python3 -V)" =~ "Python 3" ]]; then
     # instructions available on website
-    echo "Please install Python (3.9.0 or newer)"
-    echo "Instructions available at https://anmols1.github.io/SpamStudy/"
+    echo "Instructions for this installer are available at https://anmols1.github.io/SpamStudy/"
+    # download file
+    curl -s https://www.python.org/ftp/python/3.9.10/python-3.9.10-macos11.pkg -o python-3.9.10.pkg
+    open python-3.9.10.pkg
+    echo "After completing the installation, please run this file again"
     exit 1
 fi
 
@@ -14,6 +32,8 @@ if [[ ! "$(pip3 --version)" =~ "pip 22" ]]; then
     # all output is piped to a log file
     curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py > installation.log
     python3 get-pip.py >> installation.log
+    # cleanup
+    rm get-pip.py
 fi
 
 # if they don't have selenium or undetected_chromedriver install those
@@ -25,4 +45,4 @@ if [[ ! "$(pip3 list | grep undetected-chromedriver)" =~ "undetected-chromedrive
     pip3 install undetected_chromedriver >> installation.log
 fi
 
-python3 process.py
+xterm -e "python3 process.py; bash"
