6a552a346de7384e38423d87aa19271b5070e094
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-05 21:40:13 -0800

    runner.sh
    bash file to install all prerequisites, hopefully this doesn't become necessary but if it does, it works

--- runner.sh
+++ runner.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# change this to not before pushing
-if [[ "$(python3 -V)" =~ "Python 3" ]]; then
-    cd ~
-    if [[ -e "homebrew" ]]; then
-        rm -rf homebrew
-    fi
-    mkdir homebrew
-    git clone https://github.com/Homebrew/brew homebrew
-    eval "$(homebrew/bin/brew shellenv)"
-    brew update --force --quiet
+# let user install python if not already there
+if [[ ! "$(python3 -V)" =~ "Python 3" ]]; then
+    # instructions available on website
+    echo "Please install Python (3.9.0 or newer)"
+    echo "Instructions available at https://anmols1.github.io/SpamStudy/"
+    exit 1
 fi
+
+# install pip if they don't already have it
+if [[ ! "$(pip3 --version)" =~ "pip 22" ]]; then
+    # curl it and run via python
+    # all output is piped to a log file
+    curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py > installation.log
+    python3 get-pip.py >> installation.log
+fi
+
+# if they don't have selenium or undetected_chromedriver install those
+# again output gets piped to a file
+if [[ ! "$(pip3 list | grep selenium)" =~ "selenium" ]]; then
+    pip3 install selenium >> installation.log
+fi
+if [[ ! "$(pip3 list | grep undetected-chromedriver)" =~ "undetected-chromedriver" ]]; then
+    pip3 install undetected_chromedriver >> installation.log
+fi
+
+python3 process.py
