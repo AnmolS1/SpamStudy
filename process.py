efe9f66438874cebfdd01c12bcaf4943c4065682
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-22 00:12:35 -0500

    hash
    an attempt to keep info more secure

--- process.py
+++ process.py
@@ -8,13 +8,18 @@ from selenium.webdriver.common.by import By
 from ibmcloudant.cloudant_v1 import Document, CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-# comment the triple quotes to run locally
-# make throwaway acc to handle prod servicing
-'''
+# retrieve environment variables
+db = {}
 def create_vars():
 	from dotenv import load_dotenv
+	import base64
+	
 	load_dotenv()
-'''
+	
+	temp_url = os.environ['DB_URL']
+	temp_key = os.environ['DB_KEY']
+	db['URL'] = base64.b64decode(f"{temp_url}{'=' * (4 - len(temp_url) % 4)}").decode('ascii')
+	db['KEY'] = base64.b64decode(f"{temp_key}{'=' * (4 - len(temp_key) % 4)}").decode('ascii')
 
 # sleep method, makes main code a little more readable
 def sleep (n):
@@ -23,23 +28,24 @@ def sleep (n):
 # upload to database
 def upload_these(user, spam_emails):
 	# authenticate the connection
-	authenticator = IAMAuthenticator(os.environ['DB_KEY'])
+	authenticator = IAMAuthenticator(db['KEY'])
 	service = CloudantV1(authenticator=authenticator)
-	service.set_service_url(os.environ['DB_URL'])
+	service.set_service_url(db['URL'])
 	
-	# generate JSON based on the array we've set up and keep counts
+	# generate JSON based on the array we've set up in run and get counts
 	content = []
 	user_filtered = 0
 	for email in spam_emails:
 		user_filtered = user_filtered + (1 if 'user-filtered' == email[0] else 0)
 		content.append({
-			"category": email[0],
-			"from": email[1],
-			"to": email[2],
-			"time_stamp": email[3],
-			"label": email[4],
-			"content": email[5]
+			"category": email[0],    # category of email ('user-filtered' or 'gmail-filtered')
+			"from": email[1],        # who the email was from
+			"to": email[2],          # who the email is to (might seem redundant but will be useful in frontend)
+			"time_stamp": email[3],  # time stamp of email
+			"label": email[4],       # label associated with the email
+			"content": email[5]      # html content of the email (again useful in frontend)
 		})
+	# life hack: subtract to get gmail number
 	gmail_filtered = len(content) - user_filtered
 	
 	# create the document to upload
