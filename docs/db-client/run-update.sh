6d170288c4baef47a4efbd07cf21b9f2c82c68b5
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-25 04:55:01 -0500

    created shell and python files
    i made these files along with a crontab setup to automatically update the json every 5 minutes. the next part of this i'd like to try is using a listener to detect when the cloud gets updated so that processing power isn't wasted too much

--- docs/db-client/run-update.sh
+++ docs/db-client/run-update.sh
@@ -2,5 +2,5 @@ cd /Users/anmolu/GitHub/SpamStudy/docs/db-client
 python3 update-json.py
 
 git add .
-git commit -m "updating from cloud"
-git push -u origin
+git commit -m "updating json from cloud"
+git push
