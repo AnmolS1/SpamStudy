943e81c710beca62453158e7134f2e0c1fec09c7
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-28 14:05:02 -0700

    updating json from cloud

--- docs/db-client/script.js
+++ docs/db-client/script.js
@@ -9,11 +9,19 @@ const getData = () => {
     }).then(function (resJSON) {
         console.log (resJSON);
         // maybe build up using other mappings to create the final list
-        const items = resJSON.map((element) => 
+        /*const items = resJSON.map((element) => 
             React.createElement('div', { key: element._id },
                 React.createElement('h1', null, element._id)
             )
-        );
+        );*/
+
+        var items = [];
+
+        for (var i = 0; i < resJSON.length; i++) {
+            items.append (
+                React.createElement('div', null, resJSON[i]._id)
+            );
+        }
 
         ReactDOM.render(
             React.createElement('div', null, items),
