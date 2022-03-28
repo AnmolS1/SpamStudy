d1b9714809f22c4b1f30a7ba8180bc594474b0ed
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-28 13:30:03 -0700

    updating json from cloud

--- docs/db-client/script.js
+++ docs/db-client/script.js
@@ -7,8 +7,10 @@ const getData = () => {
     }).then(function (response) {
         return response.json();
     }).then(function (resJSON) {
+
         const items = resJSON.map((element) => React.createElement('h1', null, element._id));
         ReactDOM.render( React.createElement('div', null, items), document.getElementById('content'));
+        
     });
 }
 
