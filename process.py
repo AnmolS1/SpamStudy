077ef0a0c733eb5ba95f8dee531d2a902db9549d
Anmol Saxena <anmolsaxena456@gmail.com>
2022-03-30 23:03:48 -0700

    mistAke
    i left the app_icon as resources/logo.png and that won't exist when the file runs hehe oops

--- process.py
+++ process.py
@@ -13,7 +13,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def notification(message, title):
 	from desktop_notifier import DesktopNotifier
 	
-	notify = DesktopNotifier(app_name='SpamStudy', app_icon='resources/logo.png')
+	notify = DesktopNotifier(app_name='SpamStudy')
 	notify.send_sync(title=title, message=message)
 
 # sleep method, makes main code a little more readable
