286236351d9a95f01f2362ecb68d7ac425feb6eb
Anmol Saxena <anmolsaxena456@gmail.com>
2022-05-03 12:47:22 -0700

    got emails
    ok i got the emails, next step is obtaining the data from the emails. solid progress in the last 25 hours, we got this. only 143 hours left.

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+/* -------------------- BASIC DOC SCRIPTS -------------------- */
+
 $(document).ready(function () {
 	$('a[href^="#"').on('click', function (e) {
 		e.preventDefault();
@@ -22,6 +24,8 @@ $(window).scroll(function () {
 	}
 });
 
+/* -------------------- PROCESSING SCRIPTS -------------------- */
+
 gapi.load("client:auth2", function() {
 	gapi.auth2.init({client_id: config.client_id});
 });
@@ -44,25 +48,61 @@ function authenticate() {
 
 function loadClient() {
 	gapi.client.setApiKey(config.apiKey);
-	return gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1")
-		.then(function() {
-			console.log("GAPI client loaded for API");
-		}, function(err) {
-			console.error("Error loading GAPI client for API", err);
-		});
-}
-
-function execute() {
-	return gapi.client.gmail.users.messages.list({
-		'userId': 'me'
-	}).then(function(response) {
-		// handle the results here (response.result has the parsed body).
-		console.log("Response", response);
+	return gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1").then(function() {
+		console.log("GAPI client loaded for API");
 	}, function(err) {
-		console.error("Execute error", err);
+		console.error("Error loading GAPI client for API", err);
 	});
 }
 
+function getjsondata(isSpam, page_id) {
+	return {
+		'userId': 'me',
+		'includeSpamTrash': isSpam,
+		'labelIds': [isSpam ? 'SPAM' : 'INBOX'],
+		'maxResults': 500,
+		'pageToken': page_id
+	};
+}
+
+var spam_msgs = [];
+function getspamlist(page_id = '0') {
+	return gapi.client.gmail.users.messages.list(getjsondata(true, page_id))
+	.then(function(response) {
+		for (var i = 0; i < response.result.messages.length; i++)
+			spam_msgs.push(response.result.messages[i].id);
+		
+		if (response.result.hasOwnProperty('nextPageToken')) {
+			getspamlist(response.result.nextPageToken);
+		}
+	});
+}
+
+var inbox_msgs = [];
+function getinboxlist(page_id = '0') {
+	return gapi.client.gmail.users.messages.list(getjsondata(false, page_id))
+	.then(function(response) {
+		for (var i = 0; i < response.result.messages.length; i++)
+			inbox_msgs.push(response.result.messages[i].id);
+		
+		if (response.result.hasOwnProperty('nextPageToken')) {
+			getinboxlist(response.result.nextPageToken);
+		}
+	});
+}
+
+async function sleep() {
+	await new Promise(r => setTimeout(r, 5000));
+	return 0;
+}
+
 $("#start").on('click', function (e) {
-	authenticate().then(loadClient).then(execute);
+	// to get user count: check blocked list and ids in history
+	spam_msgs = [];
+	inbox_msgs = [];
+	authenticate()
+		.then(loadClient)
+		.then(getspamlist)
+		.then(getinboxlist)
+		.then(sleep);
 });
