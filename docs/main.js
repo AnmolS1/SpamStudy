7f4fd6b22b6e8872fabcffd280e4a08ee48788dc
Anmol Saxena <anmolsaxena456@gmail.com>
2022-05-04 01:01:46 -0700

    more done
    finished tidying the data, it's practically ready to upload to the cloud. i'll make changes based on whatever dr. munyaka wants tomorrow. final step is uploading the data to the ibm database. honestly this was incredible. i haven't slept since sunday night (it's wednesday morning now), and i've hardly eaten. it's just been pushing and pushing to get this done before monday next week, and it looks like i just might be able to do this And get all my other work done. props to me lol.

--- docs/main.js
+++ docs/main.js
@@ -24,15 +24,23 @@ function getInboxList(page_id = '0') {
 	});
 }
 
+
+
 const today = new Date();
 function append(isSpam, index) {
 	return gapi.client.gmail.users.messages.get({
 		'userId': 'me',
 		'id': isSpam ? spam_msgs[index] : inbox_msgs[index]
 	}).then(function(response) {
-		var date = response.result.payload.headers[1].value;
-		date = Date.parse(date.substring(date.indexOf(";") + 1).trim());
-
+		var headers = response.result.payload.headers;
+		var date;
+		for (i in headers) {
+			if (headers[i].name == 'Date') {
+				date = Date.parse(headers[i].value);
+				break;
+			}
+		}
+		
 		if (Math.floor((today - date) / 86400000.) < 31) {
 			if (isSpam) {
 				spam_msgs[index] = response;
@@ -61,10 +69,89 @@ function removeOutOfDate() {
 	inbox_msgs = inbox_msgs.filter(function(value, index, inbox_msgs) {
 		return typeof(inbox_msgs[index]) != 'string';
 	});
+
+	return 0;
+}
+
+var user_filtered_ids = [];
+function getHistory() {
+	var lowest_history = Infinity;
+	for (var i = 0; i < spam_msgs.length; i++) {
+		lowest_history = Math.min(lowest_history, parseInt(spam_msgs[i].result.historyId));
+	}
+
+	return gapi.client.gmail.users.history.list({
+		'userId': 'me',
+		'historyTypes': ['labelAdded'],
+		'labelId': 'SPAM',
+		'startHistoryId': lowest_history
+	}).then(function(response) {
+		for (var i = 0; i < response.result.history.length; i++) {
+			user_filtered_ids.push(response.result.history[i].messages[0].id);
+		}
+	})
+}
+
+function normalizeData() {
+	for (var i = 0; i < spam_msgs.length; i++) {
+		var headers = spam_msgs[i].result.payload.headers;
+		var date, foundDate = false;
+		var from, foundFrom = false;
+		for (var el in headers) {
+			if (!foundDate && headers[el].name == 'Date') {
+				date = headers[el].value;
+				foundDate = true;
+			}
+			if (!foundFrom && headers[el].name == 'From') {
+				from = headers[el].value;
+				from = from.substring(from.indexOf('<') + 1, from.indexOf('>'));
+				foundFrom = true;
+			}
+		}
+		var content = spam_msgs[i].result.payload.hasOwnProperty("parts") ? spam_msgs[i].result.payload.parts[1].body.data : spam_msgs[i].result.payload.body.data;
+
+		var data = {
+			'category': (user_filtered_ids.includes(spam_msgs[i].result.id) ? 'user' : 'gmail'),
+			'from': from,
+			'time_stamp': date,
+			'content': content,
+			'unread': spam_msgs[i].result.labelIds.includes('UNREAD')
+		}
+
+		spam_msgs[i] = data;
+	}
+
+	for (var i = 0; i < inbox_msgs.length; i++) {
+		var headers = inbox_msgs[i].result.payload.headers;
+		var date, foundDate = false;
+		var from, foundFrom = false;
+		for (var el in headers) {
+			if (!foundDate && headers[el].name == 'Date') {
+				date = headers[el].value;
+				foundDate = true;
+			}
+			if (!foundFrom && headers[el].name == 'From') {
+				from = headers[el].value;
+				from = from.indexOf("<") != -1 ? from.substring(from.indexOf('<') + 1, from.indexOf('>')) : from;
+				foundFrom = true;
+			}
+		}
+		var data = {
+			'from': from,
+			'time_stamp': date,
+			'content': content,
+			'unread': inbox_msgs[i].result.labelIds.includes('UNREAD')
+		}
+
+		inbox_msgs[i] = data;
+	}
+
+	console.log(spam_msgs);
+	console.log(inbox_msgs);
 }
 
 $("#start").on('click', function (e) {
-	// to get user count: check blocked list and ids in history
+	// last step is uploading to cloud, lessgetit
 	spam_msgs = [];
 	inbox_msgs = [];
 
@@ -76,5 +163,7 @@ $("#start").on('click', function (e) {
 		.then(getAllMessages)
 		.then(sleep)
 		.then(removeOutOfDate)
-		.then(sleep);
+		.then(getHistory)
+		.then(sleep)
+		.then(normalizeData);
 });
